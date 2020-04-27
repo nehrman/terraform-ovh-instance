8715515557d8a9c90ab47bd1522a891c060a3706
nehrman <nicolas.ehrman@gmail.com>
2020-04-27 16:29:24 +0200

    adding Bolt installation on local TF Worker

---
+++ files/scripts_bolt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install wget,
+wget https://apt.puppet.com/puppet-tools-release-xenial.deb,
+sudo dpkg -i puppet-tools-release-xenial.deb,
+sudo apt-get update,
+sudo apt-get install puppet-bolt,
+mkdir -p $HOME/.puppetlabs/bolt/,
+cat << EOF > ~/.puppetlabs/bolt/bolt.yaml
+modulepath: \"~/.puppetlabs/bolt-code/modules:~/.puppetlabs/bolt-code/site-modules\"
+inventoryfile: \"~/.puppetlabs/bolt/inventory.yaml\"
+concurrency: 10
+format: human
+ssh:
+    host-key-check: false
+    user: root
+    private-key: ~/.ssh/bolt_id 
+EOF
+cat << EOF > ~/.puppetlabs/bolt/inventory.yaml
+---
+config:
+    ssh:
+        host-key-check: false
+        user: root
+        private-key: ~/.ssh/bolt_id
+EOF,
+cat << EOF > ~/.puppetlabs/bolt/Puppetfile
+# Modules from the Puppet Forge.
+mod 'danieldreier/autosign'
+mod 'puppetlabs/puppet_agent'
+EOF,
+bolt puppetfile install
--- main.tf
+++ main.tf
@@ -6,10 +6,32 @@ resource "openstack_compute_instance_v2" "vm" {
   name            = var.instance_name
   image_id        = data.openstack_images_image_v2.vm.id
   flavor_id       = data.openstack_compute_flavor_v2.vm.id
-  key_pair         = openstack_compute_keypair_v2.vm.name
+  key_pair        = openstack_compute_keypair_v2.vm.name
   security_groups = var.security_groups
 
   network {
     name = var.network_name
   }
+}
+
+resource "null_resource" "vm" {
+
+  provisioner "local-exec" {
+    command = "echo -e openstack_compute_keypair_v2.vm.private_key > $HOME/.ssh/priv_key && chmod 0400 $HOME/.ssh/priv_key"
+  }
+
+  provisioner "file" {
+    source      = file("${path.module}/files/scripts_bolt.sh")
+    destination = "$HOME/scripts_bolt.sh"
+  }
+
+
+  /*     provisioner "puppet" "web" {
+        server = "puppetmaster.lab.deploy.ovh.net"
+        server_user = "root"
+        os_type = "linux"
+        environment = "production"
+        autosign = true
+        open_source = true
+    } */
 }
